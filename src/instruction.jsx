7ff29e323efce24c3e354266da21e3eea38355f7
Lee <leesjensen@gmail.com>
2025-08-14 08:02:19 -0600

    cleanup() unused import

--- src/instruction.jsx
+++ src/instruction.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { useSwipeNavigation } from './useSwipeNavigation';
 import mermaid from 'mermaid';
 import 'github-markdown-css/github-markdown-light.css';
