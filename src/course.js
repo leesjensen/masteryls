4e652b4ab070be078a350ee302439348ff84a4f3
Lee <leesjensen@gmail.com>
2025-08-16 08:28:30 -0600

    refactor(course) better request path

--- src/course.js
+++ src/course.js
@@ -1,6 +1,6 @@
 export default class Course {
   static async create(config) {
-    const modules = await Course._load(config);
+    const modules = await load(config);
     return new Course(config, modules);
   }
 
@@ -172,87 +172,87 @@ export default class Course {
   }
 
   async makeRequest(url, method = 'GET', body = null) {
-    const request = {
-      method,
-      headers: {
-        Authorization: `Bearer ${this.config.github.token}`,
-        Accept: 'application/vnd.github.v3+json',
-      },
-    };
-    if (body) {
-      request.headers['Content-Type'] = 'application/json';
-      request.body = JSON.stringify(body);
-    }
-    const response = await fetch(url, request);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
-    }
-    return response;
+    return makeARequest(this.config.github.token, url, method, body);
   }
+}
 
-  static async _load(config) {
-    const response = await fetch(`${config.links.gitHub.apiUrl}/instruction/modules.md`, {
-      headers: {
-        Authorization: `Bearer ${config.github.token}`,
-      },
-    });
-
-    const fileData = await response.json();
-    const markdownContent = new TextDecoder('utf-8').decode(Uint8Array.from(atob(fileData.content), (c) => c.charCodeAt(0)));
-
-    const instructionUrl = `${config.links.gitHub.apiUrl}/instruction/`;
-    return Course._parseModulesMarkdown(config, instructionUrl, markdownContent);
+async function makeARequest(token, url, method = 'GET', body = null) {
+  const request = {
+    method,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/vnd.github.v3+json',
+    },
+  };
+  if (body) {
+    request.headers['Content-Type'] = 'application/json';
+    request.body = JSON.stringify(body);
   }
+  const response = await fetch(url, request);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
+  }
+  return response;
+}
 
-  static _parseModulesMarkdown(config, instructionUrl, markdownContent) {
-    const lines = markdownContent.split('\n');
+async function load(config) {
+  const response = await makeARequest(config.github.token, `${config.links.gitHub.apiUrl}/instruction/modules.md`);
 
-    const modules = [
-      {
-        title: 'Course info',
-        topics: [
-          { title: 'Home', path: `${config.links.gitHub.apiUrl}/README.md` },
-          { title: 'Syllabus', path: config.links.syllabus || `${config.links.gitHub.apiUrl}/syllabus/syllabus.md` },
-          { title: 'Schedule', path: config.links.schedule || `${config.links.gitHub.apiUrl}/schedule/schedule.md` },
-        ],
-      },
-    ];
-    let currentModule = null;
+  const fileData = await response.json();
+  const markdownContent = new TextDecoder('utf-8').decode(Uint8Array.from(atob(fileData.content), (c) => c.charCodeAt(0)));
 
-    const moduleRegex = /^##\s+(.*)$/;
-    const topicRegex = /^-\s(.*\s)?\[(.+?)\]\((.+?)\)$/;
+  const instructionUrl = `${config.links.gitHub.apiUrl}/instruction/`;
+  return parseModulesMarkdown(config, instructionUrl, markdownContent);
+}
 
-    for (const line of lines) {
-      const moduleMatch = line.match(moduleRegex);
-      if (moduleMatch) {
-        if (currentModule) {
-          modules.push(currentModule);
-        }
-        currentModule = {
-          title: moduleMatch[1].trim(),
-          topics: [],
-        };
-        continue;
+function parseModulesMarkdown(config, instructionUrl, markdownContent) {
+  const lines = markdownContent.split('\n');
+
+  const modules = [
+    {
+      title: 'Course info',
+      topics: [
+        { title: 'Home', path: `${config.links.gitHub.apiUrl}/README.md` },
+        { title: 'Syllabus', path: config.links.syllabus || `${config.links.gitHub.apiUrl}/syllabus/syllabus.md` },
+        { title: 'Schedule', path: config.links.schedule || `${config.links.gitHub.apiUrl}/schedule/schedule.md` },
+      ],
+    },
+  ];
+  let currentModule = null;
+
+  const moduleRegex = /^##\s+(.*)$/;
+  const topicRegex = /^-\s(.*\s)?\[(.+?)\]\((.+?)\)$/;
+
+  for (const line of lines) {
+    const moduleMatch = line.match(moduleRegex);
+    if (moduleMatch) {
+      if (currentModule) {
+        modules.push(currentModule);
       }
-
-      const topicMatch = line.match(topicRegex);
-      if (topicMatch && currentModule) {
-        let prefix = topicMatch[1] ? topicMatch[1] : '';
-        let title = topicMatch[2] ? topicMatch[2].trim() : '';
-        let relPath = topicMatch[3] ? topicMatch[3].trim() : '';
-        const isAbsoluteUrl = /^(?:[a-z]+:)?\/\//i.test(relPath);
-        const path = isAbsoluteUrl ? relPath : new URL(relPath, instructionUrl).toString();
-        currentModule.topics.push({
-          title: `${prefix}${title}`,
-          path: path,
-        });
-      }
+      currentModule = {
+        title: moduleMatch[1].trim(),
+        topics: [],
+      };
+      continue;
     }
 
-    if (currentModule) {
-      modules.push(currentModule);
+    const topicMatch = line.match(topicRegex);
+    if (topicMatch && currentModule) {
+      let prefix = topicMatch[1] ? topicMatch[1] : '';
+      let title = topicMatch[2] ? topicMatch[2].trim() : '';
+      let relPath = topicMatch[3] ? topicMatch[3].trim() : '';
+      const isAbsoluteUrl = /^(?:[a-z]+:)?\/\//i.test(relPath);
+      const path = isAbsoluteUrl ? relPath : new URL(relPath, instructionUrl).toString();
+      currentModule.topics.push({
+        title: `${prefix}${title}`,
+        path: path,
+      });
     }
-
-    return modules;
   }
+
+  if (currentModule) {
+    modules.push(currentModule);
+  }
+
+  return modules;
 }
