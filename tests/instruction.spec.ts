942fcf369e1eca0d935b7f894f1d2a53bd206b52
Lee <leesjensen@gmail.com>
2025-08-22 14:36:50 -0600

    fix(test) basic display working

--- tests/instruction.spec.ts
+++ tests/instruction.spec.ts
@@ -1,51 +1,43 @@
 import { test, expect } from '@playwright/test';
 
-test.use({ serviceWorkers: 'block' });
-
-test('test', async ({ context, page }) => {
-  await page.route('*/**/course', (route) => {
-    console.log(`***********************LOADED ${route.request().url()}****************************`);
-    route.fulfill({
-      status: 200,
-      contentType: 'application/json',
-      json: {
-        title: 'QA & DevOps',
-        schedule: 'schedule/schedule.md',
-        syllabus: 'instruction/syllabus/syllabus.md',
-        links: {
-          canvas: 'https://byu.instructure.com/courses/31151',
-          chat: 'https://discord.com/channels/748656649287368704',
-          gitHub: {
-            url: 'https://github.com/devops329/devops/blob/main',
-            apiUrl: 'https://api.github.com/repos/devops329/devops/contents',
-            rawUrl: 'https://raw.githubusercontent.com/devops329/devops/main',
-          },
-        },
-        modules: [
-          {
-            title: 'Course info',
-            topics: [
-              {
-                title: 'Home',
-                path: 'README.md',
-                id: '690b3872aab6442fac17c6730d7502ed',
-              },
-              {
-                title: 'Syllabus',
-                path: 'instruction/syllabus/syllabus.md',
-                id: 'eb84677c4b7c47b1a40402c63ed07db8',
-              },
-              {
-                title: 'Schedule',
-                path: 'schedule/schedule.md',
-                id: '4d398bdff38f40108c07a336314519d4',
-              },
-            ],
-          },
-        ],
+test('basic display', async ({ page }) => {
+  const courseResponse = {
+    title: 'QA & DevOps',
+    schedule: 'schedule/schedule.md',
+    syllabus: 'instruction/syllabus/syllabus.md',
+    links: {
+      canvas: 'https://byu.instructure.com/courses/31151',
+      chat: 'https://discord.com/channels/748656649287368704',
+    },
+    modules: [
+      {
+        title: 'Module 1',
+        topics: [{ title: 'Home', path: 'something/more/README.md' }],
       },
-    });
+    ],
+  };
+
+  await page.route('*/**/course.json', async (route) => {
+    console.log('course.json!!!!!!!!!!!!');
+    expect(route.request().method()).toBe('GET');
+    await route.fulfill({ json: courseResponse });
+  });
+
+  await page.route('*/**/README.md', async (route) => {
+    console.log('readme!!!!!!!!!!!!');
+    expect(route.request().method()).toBe('GET');
+    await route.fulfill({ body: '# Home\n\nsource markdown!' });
+  });
+
+  await page.route('*/**/markdown', async (route) => {
+    console.log('markdown!!!!!!!!!!!!');
+    expect(route.request().method()).toBe('POST');
+    await route.fulfill({ body: '<h1>Home</h1><p>rendered markdown</p>' });
   });
 
   await page.goto('http://localhost:5173/');
+  await expect(page.getByRole('banner')).toContainText('💡 QA & DevOps');
+  await expect(page.getByText('▶')).toBeVisible();
+  await expect(page.getByText('Module 1')).toBeVisible();
+  await expect(page.getByText('rendered markdown')).toBeVisible();
 });
