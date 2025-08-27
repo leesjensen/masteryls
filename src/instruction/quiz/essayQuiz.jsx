680f723a03e924165a9993da06244cb049c7b206
Lee <leesjensen@gmail.com>
2025-08-26 21:47:27 -0600

    style(quiz) minor changes

--- src/instruction/quiz/essayQuiz.jsx
+++ src/instruction/quiz/essayQuiz.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function EssayQuiz({ meta }) {
-  return <textarea name={`quiz-${meta.id}`} className="w-full h-32 p-3 border border-gray-300 rounded-lg resize-none transition-colors duration-200 placeholder-gray-400" placeholder="Enter your answer here..."></textarea>;
+  return <textarea name={`quiz-${meta.id}`} className="w-full h-32 p-3 border bg-white border-gray-300 rounded-lg resize-none transition-colors duration-200 placeholder-gray-400" placeholder="Enter your answer here..."></textarea>;
 }
--- src/instruction/quiz/quizInstruction.jsx
+++ src/instruction/quiz/quizInstruction.jsx
@@ -37,7 +37,7 @@ export default function QuizInstruction(props) {
     }
     let controlJsx = generateControlComponent(meta, itemsText);
     return (
-      <div className="p-2 rounded border shadow-sm overflow-x-auto break-words whitespace-pre-line" data-plugin-masteryls data-plugin-masteryls-root data-plugin-masteryls-id={meta.id} data-plugin-masteryls-title={meta.title} data-plugin-masteryls-type={meta.type}>
+      <div className="p-2 rounded border border-gray-300 shadow-sm overflow-x-auto break-words whitespace-pre-line" data-plugin-masteryls data-plugin-masteryls-root data-plugin-masteryls-id={meta.id} data-plugin-masteryls-title={meta.title} data-plugin-masteryls-type={meta.type}>
         <fieldset>
           {meta.title && <legend className="font-semibold mb-3 break-words whitespace-pre-line">{meta.title}</legend>}
           {meta.body && <p className="mb-3 break-words whitespace-pre-line">{inlineLiteMarkdown(meta.body)}</p>}
