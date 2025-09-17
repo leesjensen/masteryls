5d8851f99ef183460a6d4a9f6bbad5fcf69ef184
Lee <leesjensen@gmail.com>
2025-09-17 17:46:28 -0600

    remove() dead code

--- src/components/EditableTopicItem.jsx
+++ src/components/EditableTopicItem.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import TopicItem from './TopicItem';
-import { TopicIcon } from './TopicIcon';
 
 export function EditableTopicItem({ id, moduleIndex, topicIndex, onRemoveTopic, onRenameTopic, topic, ...props }) {
   const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({ id });
