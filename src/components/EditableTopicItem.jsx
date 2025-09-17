facf434b2a170c9b7f9d6325a6c6c37dddea9c9e
Lee <leesjensen@gmail.com>
2025-09-17 16:22:58 -0600

    add(editor) rename topic

--- src/components/EditableTopicItem.jsx
+++ src/components/EditableTopicItem.jsx
@@ -17,12 +17,13 @@ export function EditableTopicItem({ id, moduleIndex, topicIndex, onRemoveTopic, 
       <div className="flex items-right justify-between">
         <TopicItem {...props} />
         <div className="flex flex-row justify-between">
-          <div className="ml-2 flex items-center">
-            <button onClick={() => onRemoveTopic(moduleIndex, topicIndex)} className="pr-2 font-semibold  text-gray-400 hover:text-red-600" title="Remove this topic">
-              x
-            </button>
-          </div>
-          <span {...listeners} className="mr-2 select-none font-semibold text-gray-400 hover:text-amber-500 border-[1px] px-1 border-gray-300" title="Drag to reorder">
+          <button onClick={() => onRenameTopic(moduleIndex, topicIndex)} className="font-semibold  text-gray-400 hover:text-blue-600 border-1 px-1 border-gray-300" title="Edit this topic">
+            e
+          </button>
+          <button onClick={() => onRemoveTopic(moduleIndex, topicIndex)} className="font-semibold  text-gray-400 hover:text-red-600 border-1 px-1 border-gray-300" title="Remove this topic">
+            x
+          </button>
+          <span {...listeners} className="select-none font-semibold text-gray-400 hover:text-amber-500 border-1 px-1 border-gray-300" title="Drag to reorder">
             ≡
           </span>
         </div>
