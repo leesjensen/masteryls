77dc548d9c29e3575fa4f57c0fe6e897cb091282
Lee <lee.jensen@agilix.com>
2025-10-06 17:43:35 -0600

    enhance(hero) load from server not from 3rd party

--- src/start.jsx
+++ src/start.jsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Login from './login.jsx';
 
 const stockImages = {
-  hero: 'https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixlib=rb-4.0.3&auto=format&fit=crop&w=1470&q=80',
-
+  hero: 'masteryls-hero.avif',
   instruction: 'https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-4.0.3&auto=format&fit=crop&w=1470&q=80',
   aiLearning: 'https://images.unsplash.com/photo-1513258496099-48168024aec0?ixlib=rb-4.0.3&auto=format&fit=crop&w=1470&q=80',
   aiContent: 'https://images.unsplash.com/photo-1677442136019-21780ecad995?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&h=980',
