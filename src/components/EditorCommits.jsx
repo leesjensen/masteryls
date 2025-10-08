4996b7907ccc1b171df3a69f322854f27ecd3495
Lee <leesjensen@gmail.com>
2025-10-08 17:26:43 -0600

    impl(editor) commits to show diff

--- src/components/EditorCommits.jsx
+++ src/components/EditorCommits.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 export default function EditorCommits({ currentTopic, course, user, service, setContent, setDirty }) {
   const [topicCommits, setTopicCommits] = useState([]);
   const [showCommits, setShowCommits] = useState(true);
+  const [currentCommit, setCurrentCommit] = useState(currentTopic.commit);
 
   // Fetch commits when dependencies change
   useEffect(() => {
@@ -28,6 +29,7 @@ export default function EditorCommits({ currentTopic, course, user, service, set
     const repoApiUrl = course.links.gitHub.apiUrl.replace(/\/contents.*/, '');
     const filePath = currentTopic.path.replace(course.links.gitHub.rawUrl + '/', '');
     const content = await service.getTopicContentAtCommit(user.getSetting('gitHubToken', course.id), repoApiUrl, filePath, commit.sha);
+    setCurrentCommit(commit.sha);
     setContent(content);
     setDirty(true);
   };
@@ -42,12 +44,19 @@ export default function EditorCommits({ currentTopic, course, user, service, set
       {showCommits && (
         <div className="max-h-64 overflow-auto border border-gray-300 rounded bg-gray-50 p-2 m-2 ">
           <ul className="text-xs">
-            {topicCommits.map((commit) => (
+            {topicCommits.map((commit, idx) => (
               <li key={commit.sha} className="mb-2">
                 <div className="flex items-center justify-start border border-gray-300 rounded p-1 w-full">
-                  <button className="mr-2 px-2 py-1 text-xs bg-blue-200 hover:bg-blue-300 rounded text-blue-900 border border-blue-300" onClick={() => handleApplyCommit(commit)}>
-                    Apply
-                  </button>
+                  {commit.sha !== currentCommit && (
+                    <>
+                      <button className="mr-2 px-2 py-1 text-xs bg-blue-200 hover:bg-blue-300 rounded text-blue-900 border border-blue-300" onClick={() => handleApplyCommit(commit)}>
+                        Apply
+                      </button>
+                      <button className="mr-2 px-2 py-1 text-xs bg-blue-200 hover:bg-blue-300 rounded text-blue-900 border border-blue-300" onClick={() => handleApplyCommit(commit)}>
+                        Diff
+                      </button>
+                    </>
+                  )}
                   <div className="flex flex-col">
                     <div className="flex flex-row">
                       <a href={commit.html_url} target="_blank" rel="noopener noreferrer" className="pr-1 text-blue-600 underline">
