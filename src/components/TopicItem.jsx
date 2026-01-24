3faf85a304bc8c987b59628dbd287b951899fb89
Lee <lee.jensen@agilix.com>
2026-01-24 16:57:23 -0700

    remove(progress) reporting based on reading progress
    #14

--- src/components/TopicItem.jsx
+++ src/components/TopicItem.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { TopicIcon } from './TopicIcon';
 import { useNavigate } from 'react-router-dom';
-import { useTopicInteractionProgress } from './instruction/interaction/interactionProgressStore';
 
 function TopicItem({ course, topic, currentTopic }) {
   const navigate = useNavigate();
-  const completionPercentage = useTopicInteractionProgress(topic.interactions);
 
   return (
     <li className="mb-0.5 flex justify-between items-center group">
@@ -20,7 +18,7 @@ function TopicItem({ course, topic, currentTopic }) {
         </span>
         <a onClick={() => navigate(`/course/${course.id}/topic/${topic.id}`)} className={`no-underline cursor-pointer truncate max-w-full block whitespace-nowrap overflow-hidden text-ellipsis flex-1 ${topic.path === currentTopic?.path ? 'text-amber-500 font-semibold' : 'text-gray-500 hover:text-amber-500'}`} title={topic.title}>
           {topic.title}
-          {!!topic.interactions?.length && ` ✨${completionPercentage}%`}
+          {!!topic.interactions?.length && ` ✨`}
         </a>
       </div>
     </li>
