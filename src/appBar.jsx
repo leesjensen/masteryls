255f887796e725cd94b0fba82e6311f3d0ee6c61
Lee <leesjensen@gmail.com>
2026-01-28 14:00:06 -0700

    enhance(appbar) to have a clean white bg that makes the icons pop

--- src/appBar.jsx
+++ src/appBar.jsx
@@ -4,8 +4,8 @@ import { useAppBarState } from './hooks/useAppBarState';
 export function AppBar() {
   const { title, tools } = useAppBarState();
   return (
-    <header className="flex flex-row items-center justify-between pr-2 border-b-1 bg-gray-600 border-gray-500 h-[42px]">
-      <h1 className="pl-1 font-medium text-lg text-gray-50 flex items-center min-w-0">
+    <header className="flex flex-row items-center justify-between pr-2 border-b-1 bg-gray-50 border-gray-500 h-[42px]">
+      <h1 className="pl-1 font-medium text-lg text-gray-900 flex items-center min-w-0">
         <span className="hidden sm:inline-flex items-center justify-center bg-white border border-gray-300 rounded-full w-[32px] h-[32px] ml-1 mr-2">
           <img src="/favicon.png" alt="MasteryLS logo" className=" w-[20px] h-[20px]" />
         </span>{' '}
@@ -18,7 +18,7 @@ export function AppBar() {
 
 export function AppBarButton({ icon: Icon, onClick, title = undefined, size = 18 }) {
   return (
-    <button title={title} onClick={onClick} className="bg-white border border-gray-50  hover:text-amber-600 transition-all duration-200 ease-in-out">
+    <button title={title} onClick={onClick} className="bg-transparent border border-gray-50  hover:text-amber-600 transition-all duration-200 ease-in-out">
       <Icon size={size} />
     </button>
   );
