f2808a999f41e0ac531d6bb1bfc362da306facbf
Lee Jensen <leesjensen@gmail.com>
2026-02-10 21:04:08 -0700

    enhance(video) to use embedded URL

--- src/components/instruction/videoInstruction.jsx
+++ src/components/instruction/videoInstruction.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
 export default function VideoInstruction({ learningSession, courseOps }) {
-  return <iframe className="w-full h-full" src={`https://www.youtube.com/embed/${extractYouTubeId(learningSession.topic?.path)}?cb=${Date.now()}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />;
-
-  function extractYouTubeId(url) {
-    const regExp = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/i;
-    const match = url.match(regExp);
-    return match ? match[1] : '';
-  }
+  return <iframe className="w-full h-full" src={learningSession.topic?.path} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />;
 }
