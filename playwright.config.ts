45e64f037f357b3e059254115a600f43dd6b2737
Lee <leesjensen@gmail.com>
2026-02-13 14:19:10 -0700

    fix(test) to wait longer for course creation

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
-  timeout: process.env.CI ? 5000 : 20000,
+  timeout: process.env.CI ? 10000 : 20000,
   use: {
     baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
